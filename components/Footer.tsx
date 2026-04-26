0574174adf7cf39da84d460faea3d22567314e1d
Stefano Natali <natali3d@gmail.com>
2026-04-26 10:05:48 +0200

    Aggiunti social nel footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,23 +4,59 @@ export default function Footer() {
       <p>Studio · Cesena (FC) · Italy</p>
       <p>Email: info@3dwork.it</p>
 
-      <div className="mt-4 flex justify-center gap-6">
+      {/* SOCIAL ICONS */}
+      <div className="mt-6 flex justify-center gap-6">
+        {/* Facebook */}
         <a
-          href="/privacy"
-          className="underline hover:text-white transition"
+          href="https://www.facebook.com/3DWORKstudio/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Facebook"
+          className="hover:text-white transition"
         >
-          Privacy Policy
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="22"
+            height="22"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path d="M22 12a10 10 0 1 0-11.5 9.9v-7h-2v-3h2v-2.3c0-2 1.2-3.1 3-3.1.9 0 1.8.1 1.8.1v2h-1c-1 0-1.3.6-1.3 1.2V12h2.3l-.4 3h-1.9v7A10 10 0 0 0 22 12" />
+          </svg>
         </a>
 
+        {/* LinkedIn */}
         <a
-          href="/cookie"
-          className="underline hover:text-white transition"
+          href="https://www.linkedin.com/in/stefano-natali-pittura-3dwork/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+          className="hover:text-white transition"
         >
-          Cookie Policy
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="22"
+            height="22"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path d="M4.98 3.5a2.5 2.5 0 1 1 0 5.001 2.5 2.5 0 0 1 0-5.001zM3 9h4v12H3zM14.5 9c-2.33 0-3.5 1.34-3.5 3.2V21h-4V9h4v1.7c.6-1 1.8-1.7 3.5-1.7 2.76 0 5 2.24 5 5v7h-4v-7c0-1.1-.9-2-2-2z" />
+          </svg>
         </a>
-      </div>
 
-      <p className="mt-4">©2026 3D WORK™ · P.I. 03371840400</p>
-    </footer>
-  );
-}
+        {/* Instagram */}
+        <a
+          href="https://www.instagram.com/3dwork.it/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram"
+          className="hover:text-white transition"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="22"
+            height="22"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+
