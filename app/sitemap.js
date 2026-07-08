811058cc2810de84d5b2df889964d8f3d34817e3
Stefano Natali <natali3d@gmail.com>
2026-07-08 08:10:09 +0200

    Aggiornata sitemap + fix robots

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,12 +1,43 @@
-export const dynamic = "force-static";
-
 export default function sitemap() {
+  const baseUrl = "https://www.3dwork.it";
+
   return [
     {
-      url: 'https://www.3dwork.it',
+      url: `${baseUrl}/`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1.0,
+      changefreq: "monthly",
+      priority: 1,
     },
+    {
+      url: `${baseUrl}/render-interni`,
+      lastModified: new Date(),
+      changefreq: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/render-esterni`,
+      lastModified: new Date(),
+      changefreq: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/render-immobili`,
+      lastModified: new Date(),
+      changefreq: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/cookie`,
+      lastModified: new Date(),
+      changefreq: "yearly",
+      priority: 0.3,
+    },
+    {
+      url: `${baseUrl}/privacy`,
+      lastModified: new Date(),
+      changefreq: "yearly",
+      priority: 0.3,
+    }
   ];
 }
+
