e72d536ad4ce23052b55f2687adf2ff8d089568f
Stefano Natali <natali3d@gmail.com>
2026-07-26 15:59:19 +0200

    Fix pagina Tour Interno con componente React valido

---
+++ app/tour360-interno/page.jsx
@@ -0,0 +1,12 @@
+export default function TourInternoPage() {
+  return (
+    <div className="w-full h-screen pt-24">
+      <iframe
+        src="/PanoTest/index.html"
+        className="w-full h-full border-0"
+      />
+    </div>
+  );
+}
+
+
