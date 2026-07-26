f49b301876398692855733acb7821f2b784cd9b4
Stefano Natali <natali3d@gmail.com>
2026-07-26 16:23:06 +0200

    Fix fullscreen pagina Interno

--- app/tour360-interno/page.jsx
+++ app/tour360-interno/page.jsx
@@ -1,14 +1,28 @@
 export default function TourInternoPage() {
   return (
-    <main style={{ width: "100%", height: "100vh" }}>
+    <main
+      style={{
+        position: "fixed",
+        top: "80px", // altezza del tuo Header (pt-20)
+        left: 0,
+        width: "100%",
+        height: "calc(100vh - 80px)",
+        zIndex: 1
+      }}
+    >
       <iframe
         src="/PanoTest/index.html"
-        style={{ border: "none", width: "100%", height: "100%" }}
+        style={{
+          border: "none",
+          width: "100%",
+          height: "100%"
+        }}
         title="Tour 360° Interno"
       />
     </main>
   );
 }
+
 
 
 
