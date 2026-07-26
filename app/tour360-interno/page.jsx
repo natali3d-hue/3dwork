51758d04531861502deb1cb97943553e77ca9456
Stefano Natali <natali3d@gmail.com>
2026-07-26 16:12:21 +0200

    Fix iframe Interno: altezza corretta per mostrare il menu

--- app/tour360-interno/page.jsx
+++ app/tour360-interno/page.jsx
@@ -1,6 +1,6 @@
 export default function TourInternoPage() {
   return (
-    <main style={{ width: "100%", height: "calc(100vh - 80px)", marginTop: "80px" }}>
+    <main style={{ width: "100%", height: "100vh" }}>
       <iframe
         src="/PanoTest/index.html"
         style={{ border: "none", width: "100%", height: "100%" }}
@@ -9,6 +9,7 @@ export default function TourInternoPage() {
     </main>
   );
 }
+
 
 
 
