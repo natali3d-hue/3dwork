76d158e12afe3552c3a094e50546216ad58eef6f
Stefano Natali <natali3d@gmail.com>
2026-07-26 16:02:49 +0200

    Fix altezza iframe per mostrare il menu nella pagina Interno

--- app/tour360-interno/page.jsx
+++ app/tour360-interno/page.jsx
@@ -1,9 +1,9 @@
 export default function TourInternoPage() {
   return (
-    <div className="w-full h-screen pt-24">
+    <div className="w-full h-screen">
       <iframe
         src="/PanoTest/index.html"
-        className="w-full h-full border-0"
+        className="w-full h-[calc(100vh-96px)] mt-24 border-0"
       />
     </div>
   );
