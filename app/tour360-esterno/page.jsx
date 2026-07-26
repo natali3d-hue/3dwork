62e454088f6780d13e51987fa5028664adae70af
Stefano Natali <natali3d@gmail.com>
2026-07-26 15:22:33 +0200

    Aggiunto PanoEsterno

---
+++ app/tour360-esterno/page.jsx
@@ -0,0 +1,12 @@
+export default function TourEsternoPage() {
+  return (
+    <main style={{ width: "100%", height: "100vh" }}>
+      <iframe
+        src="/PanoEsterno/index.html"
+        style={{ border: "none", width: "100%", height: "100%" }}
+        title="Tour 360° Esterno"
+      />
+    </main>
+  );
+}
+
---
+++ public/PanoEsterno/data.js
@@ -0,0 +1,342 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-fronte-1",
+      "name": "Fronte 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3536620968836544
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6185860973176833,
+          "pitch": 0.2985373158563309,
+          "rotation": 0,
+          "target": "0-fronte-1"
+        },
+        {
+          "yaw": -0.25183970086934515,
+          "pitch": 0.10210427324334503,
+          "rotation": 0,
+          "target": "1-fronte-2"
+        },
+        {
+          "yaw": 0.9409890178654319,
+          "pitch": 0.056796714283049,
+          "rotation": 0,
+          "target": "2-retro-1"
+        },
+        {
+          "yaw": 0.8862430948899345,
+          "pitch": 0.14305584996814424,
+          "rotation": 0,
+          "target": "5-centrale-fronte"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6970720361143865,
+          "pitch": -0.46770542840941864,
+          "title": "Case Frini, Cesena (FC)<div><br></div>",
+          "text": "Tour 360°"
+        }
+      ]
+    },
+    {
+      "id": "1-fronte-2",
+      "name": "Fronte 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.004104359311311256,
+          "pitch": 0.27026499847710994,
+          "rotation": 0,
+          "target": "1-fronte-2"
+        },
+        {
+          "yaw": 0.695864472562441,
+          "pitch": 0.08641240018884133,
+          "rotation": 0,
+          "target": "0-fronte-1"
+        },
+        {
+          "yaw": -0.5563056746857011,
+          "pitch": 0.0574473013548662,
+          "rotation": 0,
+          "target": "3-retro-2"
+        },
+        {
+          "yaw": -0.35023148852289765,
+          "pitch": 0.14923572720352496,
+          "rotation": 0,
+          "target": "4-centrale-retro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-retro-1",
+      "name": "Retro 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23254465860679296,
+          "pitch": 0.06914759646119606,
+          "rotation": 0,
+          "target": "3-retro-2"
+        },
+        {
+          "yaw": -0.48094369863807884,
+          "pitch": 0.2649109256048199,
+          "rotation": 0,
+          "target": "2-retro-1"
+        },
+        {
+          "yaw": -1.0237333807540505,
+          "pitch": 0.034462862588000576,
+          "rotation": 0,
+          "target": "0-fronte-1"
+        },
+        {
+          "yaw": -0.894366803327932,
+          "pitch": 0.11764646813062463,
+          "rotation": 0,
+          "target": "5-centrale-fronte"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-retro-2",
+      "name": "Retro 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.05707720173317732,
+        "pitch": 0.10888752652512146,
+        "fov": 1.3536620968836544
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.896772230268386,
+          "pitch": 0.06012712062380565,
+          "rotation": 0,
+          "target": "1-fronte-2"
+        },
+        {
+          "yaw": 0.4675117411052998,
+          "pitch": 0.3799637545392329,
+          "rotation": 0,
+          "target": "3-retro-2"
+        },
+        {
+          "yaw": -0.4410018826415687,
+          "pitch": 0.07778832551602832,
+          "rotation": 0,
+          "target": "2-retro-1"
+        },
+        {
+          "yaw": 0.8041660053558424,
+          "pitch": 0.16509718815117225,
+          "rotation": 0,
+          "target": "4-centrale-retro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-centrale-retro",
+      "name": "Centrale Retro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.814013100615691,
+          "pitch": 0.13168020890955745,
+          "rotation": 0,
+          "target": "1-fronte-2"
+        },
+        {
+          "yaw": -0.8493822355912162,
+          "pitch": 0.11737470407827466,
+          "rotation": 0,
+          "target": "3-retro-2"
+        },
+        {
+          "yaw": 0.006156479456960184,
+          "pitch": 0.23564132707389618,
+          "rotation": 0,
+          "target": "4-centrale-retro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-centrale-fronte",
+      "name": "Centrale Fronte",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8148595980903153,
+          "pitch": 0.10635372469739401,
+          "rotation": 0,
+          "target": "0-fronte-1"
+        },
+        {
+          "yaw": 0.8419925744206722,
+          "pitch": 0.10867032419115574,
+          "rotation": 0,
+          "target": "2-retro-1"
+        },
+        {
+          "yaw": 0.006143136437419372,
+          "pitch": 0.19607525113212,
+          "rotation": 0,
+          "target": "5-centrale-fronte"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
